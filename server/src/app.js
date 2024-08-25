76e905f16f026512869073371b4a84b673a5c4f4
PavelChernickov <chernickov.pasha@gmail.com>
2024-08-25 23:18:56 +0300

    client: react-router-dom

--- server/src/app.js
+++ server/src/app.js
@@ -3,4 +3,5 @@ require('dotenv').config()
 const allRouters = require('./routers/api')
 app = express()
 app.use('/api', allRouters)
+
 app.listen(process.env.PORT, () => {console.log('Server started on ' + process.env.PORT)})
